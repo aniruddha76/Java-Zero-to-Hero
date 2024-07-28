1e483bb77a11ef112115c7f9b7259bc2343b53c3
aniruddha <aniruddhathakur76@gmail.com>
2024-07-28 13:06:01 +0530

    Input in Java

---
+++ Calculation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Calculation {
+    public static void main(String[] args) {
+        //input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+
+        //some of the examples of input types
+
+        //nextInt()
+        //nextFloat()
+
+        //assignment
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
