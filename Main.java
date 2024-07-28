0ac1ff8b5ef8a8a43e8b1645655e67b3643b819a
aniruddha <aniruddhathakur76@gmail.com>
2024-07-28 12:55:09 +0530

    First Java File

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main{
+    public static void main(String[] args){
+        //output
+        System.out.println("*");
+
+        //variables
+        String name = "Aniruddha";
+        int a = 25;
+        int b = 10;
+        int age = 21;
+        double price = 25.25;
+        
+        System.out.println(name);
+
+        int sum = a + b + age;
+        System.out.println(sum);
+
+        System.out.println(price);
+
+        sum = a * b;
+        System.out.println(sum);
+    }
+}
